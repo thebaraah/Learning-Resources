9e9ba87c05ec01f2372152384c2295b491afc173
stasel <2033301+stasel@users.noreply.github.com>
2026-04-02 14:29:48 +0200

    Added compliments API

---
+++ core-program/week-13/compliments-api/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { compliments } from './compliments.js';
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Compliments API! Use /compliment/:name to get a personalized compliment.");
+});
+
+app.get("/compliment/:name", (req, res) => {
+  const name = req.params.name;
+  const randomIndex = Math.floor(Math.random() * compliments.length);
+  const compliment = compliments[randomIndex].replace("{name}", name)
+
+  res.json({
+    id: randomIndex + 1,
+    compliment: compliment,
+  });
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Compliments server running on http://localhost:${port}`);
+});
