1f311a0f94fee43f18a075b7f16379bc4af11c96
stasel <2033301+stasel@users.noreply.github.com>
2026-07-02 15:01:57 +0200

    Added exercise for week 10

---
+++ backend-track/week-10/passwords/src/main/java/net/hackyourfuture/Main.java
@@ -0,0 +1,8 @@
+package net.hackyourfuture;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        String hash = "$2a$08$w5Dq7PldAp1SSJLOftYCOe7waZZpNMPqtfChG0sJ3arzJRCqcN7rO";
+    }
+}
